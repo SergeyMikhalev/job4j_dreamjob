afac579647ecc1ca20c4aecd41941fb057883a60
Сергей <tolltroll@yandex.ru>
2022-10-02 21:32:52 +0300

    Posts

---
+++ src/main/java/ru/job4j/dreamjob/model/Post.java
@@ -0,0 +1,67 @@
+package ru.job4j.dreamjob.model;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Post {
+    private int id;
+    private String name;
+    private String description;
+    private LocalDateTime localDateTime;
+
+
+
+    public Post() { }
+
+    public Post(int id, String name, String description, LocalDateTime localDateTime) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.localDateTime = localDateTime;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getLocalDateTime() {
+        return localDateTime;
+    }
+
+    public void setLocalDateTime(LocalDateTime localDateTime) {
+        this.localDateTime = localDateTime;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Post post = (Post) o;
+        return id == post.id && name.equals(post.name) && description.equals(post.description) && localDateTime.equals(post.localDateTime);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, description, localDateTime);
+    }
+}
