7bbf3a15ab72f1f60c928cc08ba9f44005eb839a
SergeyMikhalev <tolltroll@yandex.ru>
2022-10-03 16:20:58 +0300

    add candidate

--- src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
+++ src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
@@ -4,7 +4,10 @@ package ru.job4j.dreamjob.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.job4j.dreamjob.model.Candidate;
 import ru.job4j.dreamjob.store.CandidateStore;
+
+import java.time.LocalDate;
 
 @Controller
 public class CandidateController {
@@ -15,5 +18,16 @@ public class CandidateController {
     public String posts(Model model) {
         model.addAttribute("candidates", store.findAll());
         return "candidates";
+    }
+
+    @GetMapping("/formAddCandidate")
+    public String addPost(Model model) {
+        model.addAttribute("candidate",
+                new Candidate(0,
+                        "Заполните поле",
+                        "Заполните поле",
+                        "Заполните поле",
+                        LocalDate.now()));
+        return "addCandidate";
     }
 }
