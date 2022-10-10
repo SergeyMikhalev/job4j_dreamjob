207024290921c9077d0777d009b907a1215dff65
SergeyMikhalev <tolltroll@yandex.ru>
2022-10-10 17:02:04 +0300

    spring singleton

--- src/main/java/ru/job4j/dreamjob/services/PostService.java
+++ src/main/java/ru/job4j/dreamjob/services/PostService.java
@@ -1,34 +1,32 @@
 package ru.job4j.dreamjob.services;
 
+import org.springframework.stereotype.Service;
 import ru.job4j.dreamjob.model.Post;
 import ru.job4j.dreamjob.store.PostStore;
 
 import java.util.Collection;
 
+@Service
 public class PostService {
-    private static final PostService INST = new PostService();
-    private final PostStore store = PostStore.instOf();
+    private final PostStore postStore;
 
-    private PostService() {
-    }
-
-    public static PostService instOf() {
-        return INST;
+    private PostService(PostStore postStore) {
+        this.postStore = postStore;
     }
 
     public Collection<Post> findAll() {
-        return store.findAll();
+        return postStore.findAll();
     }
 
     public void add(Post post) {
-        store.add(post);
+        postStore.add(post);
     }
 
     public Post findById(int id) {
-        return store.findById(id);
+        return postStore.findById(id);
     }
 
     public void update(Post post) {
-        store.update(post);
+        postStore.update(post);
     }
 }
